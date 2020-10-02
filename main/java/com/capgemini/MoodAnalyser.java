bfa6b2194b83d7f096abc58aa14753f06226a949
shreya <shreyareddykethireddy@shreyas-MacBook-Air.local>
2020-10-03 00:37:17 +0530

    [Shreya]Added:Happy or Sad

--- main/java/com/capgemini/MoodAnalyser.java
+++ main/java/com/capgemini/MoodAnalyser.java
@@ -1,12 +1,10 @@
 package com.capgemini;
 
 public class MoodAnalyser {
-    public String analyseMood(String message) {
-    	if(message.contains(("sad"))) {
-    		return "SAD";
-    	}
-    	else {
-    		return "HAPPY";
-    	}
-    }
-}
+	public String analyseMood(String message) {
+			if (message.contains("sad"))
+				return "SAD";
+			else
+				return "HAPPY";
+		}
+	}
--- test/java/com/capgemini/MoodAnalyserTest.java
+++ test/java/com/capgemini/MoodAnalyserTest.java
@@ -1,14 +1,18 @@
 package com.capgemini;
 
 import org.junit.Test;
-import org.hamcrest.CoreMatchers;
-import org.junit.*;
+
+import com.capgemini.MoodAnalyser;
+
+import static org.junit.Assert.*;
+
+import org.junit.Assert;
 
 public class MoodAnalyserTest {
-    @Test 
-	public void moodAnalysis() throws Exception {
+	@Test
+	public void TestMoodAnalysis() {
 		MoodAnalyser moodAnalyser = new MoodAnalyser();
-		String mood = moodAnalyser.analyseMood("This is a sad Message.");
-		Assert.assertThat(mood, CoreMatchers.is("SAD"));
+		String mood = moodAnalyser.analyseMood("This is a happy message");
+		Assert.assertEquals("HAPPY", mood);
 	}
 }
